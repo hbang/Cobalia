efb513d35cec51496c88a066aa47910b61f132fd
Adam Demasi <adam@hbang.com.au>
2015-10-10 20:10:18 +1030

    [prefs] localize-ify everything

--- prefs/HBCBRootListController.m
+++ prefs/HBCBRootListController.m
@@ -5,11 +5,12 @@
 #pragma mark - Constants
 
 + (NSString *)hb_shareText {
-	return [NSString stringWithFormat:@"I’m using Cobalia to quickly access toggles from my %@ app switcher!", [UIDevice currentDevice].localizedModel];
+	NSString *formatString = NSLocalizedStringFromTableInBundle(@"SHARE_TEXT", @"Root", [NSBundle bundleForClass:self.class], @"Default text for sharing the tweak. %@ is the device type (ie, iPhone).");
+	return [NSString stringWithFormat:formatString, [UIDevice currentDevice].localizedModel];
 }
 
 + (NSURL *)hb_shareURL {
-	return [NSURL URLWithString:@"https://www.hbang.ws/tweaks/cobalia"];
+	return [NSURL URLWithString:@"https://www.hbang.ws/tweaks/cobalia/"];
 }
 
 + (UIColor *)hb_tintColor {
